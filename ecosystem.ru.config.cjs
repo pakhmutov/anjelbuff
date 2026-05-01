e43ce9350022b15837d3e828247c4b63fe95991d
pahmutov <pahmutov@f5.com.ru>
2026-05-01 17:20:17 +0200

    fix: update script paths in ecosystem configuration files for consistency

--- ecosystem.ru.config.cjs
+++ ecosystem.ru.config.cjs
@@ -2,10 +2,9 @@ module.exports = {
     apps: [
         {
             name: 'ru-worker',
-            script: 'packages/ru-worker/src/index.ts',
+            script: 'src/index.ts',
             interpreter: '/root/anjelbuff/node_modules/.bin/tsx',
-            cwd: '/root/anjelbuff',
-            env_file: './packages/ru-worker/.env',
+            cwd: '/root/anjelbuff/packages/ru-worker',
             restart_delay: 5000,
             max_restarts: 10,
         },
--- ecosystem.uk.config.cjs
+++ ecosystem.uk.config.cjs
@@ -2,10 +2,9 @@ module.exports = {
     apps: [
         {
             name: 'uk-service',
-            script: 'packages/uk-service/src/index.ts',
+            script: 'src/index.ts',
             interpreter: '/root/anjelbuff/node_modules/.bin/tsx',
-            cwd: '/root/anjelbuff',
-            env_file: './packages/uk-service/.env',
+            cwd: '/root/anjelbuff/packages/uk-service',
             restart_delay: 5000,
             max_restarts: 10,
         },
